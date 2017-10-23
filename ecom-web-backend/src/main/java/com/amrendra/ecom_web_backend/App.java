606d74a4a5e3efc94661c4bec571ebac5b20082e
amrendra singh <Amrendra PC@192.168.0.8>
2017-10-23 12:07:00 -0700

    Initial Ecom Project Set Up

---
+++ ecom-web-backend/src/main/java/com/amrendra/ecom_web_backend/App.java
@@ -0,0 +1,13 @@
+package com.amrendra.ecom_web_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
